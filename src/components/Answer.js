f8595b86926b19a8bb52ecddcff5ae6af9cbf98e
Vivian Bow <vivianbao1009@gmail.com>
2022-02-21 22:22:13 +0900

    Styling tweak

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -10,13 +10,13 @@ export default function Answer(props) {
   // props.selected ? { 'background-color': '#d6dbf5', 'border': 'transparent 1px #293264' } : {'border': 'solid 1px #293264'}
   function checkedStyle(){
     if(props.correct && props.selected) {
-      return { backgroundColor: '#94d7a2', border: "none"}
+      return { backgroundColor: '#94d7a2', border: "solid 1px #f6f7fb"}
     }else if(props.correct){
-      return { backgroundColor: '#94d7a2', border: "none"}
+      return { backgroundColor: '#94d7a2', border: "solid 1px #f6f7fb"}
     } else if(props.selected){
-      return { backgroundColor: '#f7d9db', border: "none", color: '#8f95af' }
+      return { backgroundColor: '#f7d9db', border: "solid 1px #f6f7fb", color: '#8f95af' }
     } else {
-      return { color: "#8f95af"}
+      return { color: "#8f95af", border: "solid 1px #8f95af"}
     }
   }
   console.log(props.checked)
